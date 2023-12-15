fbdb4a508d626d5c6ca473c6e1abb52ea16c776f
CadmanW <cadmanw13@gmail.com>
2023-12-15 11:33:43 -0600

    update CyberPatriot Extension
    trying to get the svg, ran into a wall. Don't know how to proceed

--- CyberPatriot Extention V2/content.js
+++ CyberPatriot Extention V2/content.js
@@ -3,16 +3,16 @@ function initPanel() {
     const mainEl = document.querySelectorAll(".main")[1];
     mainEl.style.margin = "0px 0px 0px 30px";
     mainEl.style.float = "left";
-    mainEl.style.width = "700px";
+    mainEl.style.width = "50%";
 
     // Create the panel then append it to the document
-    const height = (document.body.scrollHeight - 179).toString() + "px";
+    const height = (document.body.scrollHeight - 180).toString() + "px";
     let generateTime = document.querySelector("h4").textContent;
 
     const panelEl = document.createElement("div");
     panelEl.id = "06132007";
     panelEl.style.minHeight = height;
-    panelEl.style.width = "1110px";
+    panelEl.style.width = "43%";
     panelEl.style.backgroundColor = "white";
     panelEl.style.float = "right";
     panelEl.style.margin = "0px 30px 0px 0px";
@@ -31,29 +31,37 @@ function initPanel() {
     mainEl.insertAdjacentElement("afterend", panelEl);
 }
 
+function getGraph(DOM) {
+    const container = document.createElement("div");
+    const svg = DOM.querySelector("svg");
+
+    container.id = "char_div";
+
+    container.appendChild(svg);
+
+    return container;
+}
+
 function getData(url) {
     fetch(url, {"method": "GET"}).then(re => {return re.text()}).then(html => {
         const document2 = new DOMParser().parseFromString(html, "text/html");
         const panel = document.querySelector("div[id='06132007']");
         const dataEl = document2.querySelector("div[class='container-fluid']");
-        const graph = document2.querySelector("div[id='chart_div']");
+        const graph = getGraph(document2);
 
-        dataEl.querySelector("p[class='disclaimer']").remove();
-        dataEl.querySelector("div[class='chart']").remove();
+        dataEl.querySelector(".disclaimer").remove();
+        dataEl.querySelector(".chart").remove();
 
-        const containerEl = document.createElement("div");
+        const containerEl = document2.createElement("div");
         containerEl.id = id;
         id++;
         containerEl.style.boxSizing = "border-box";
         containerEl.style.border = "solid 2px rgb(60, 60, 60)";
         containerEl.style.borderTop = "solid 10px rgb(40, 40, 40)";
 
-        console.log(dataEl);
-        console.log(graph);
-
-        panel.appendChild(containerEl);
-        containerEl.appendChild(dataEl);
         dataEl.appendChild(graph);
+        containerEl.appendChild(dataEl);
+        panel.appendChild(containerEl);
     });
 }
 
